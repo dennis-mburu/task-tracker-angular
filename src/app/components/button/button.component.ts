8cfd7a0e62738f2939a01560c8ae4e396a75889e
Dennnis Mburu Mwaura <dennismburu2063@gmail.com>
2023-09-02 08:46:58 +0300

    add task interface and mock task data

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -6,9 +6,12 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   styleUrls: ['./button.component.css'],
 })
 export class ButtonComponent {
+
+  // TODO: Find out why if I remove the ! following color I get an error message
   @Input() color!: string;
   @Input() text!: string;
 
+  // TODO: check out event emmiters in angular
   @Output() btnClick = new EventEmitter();
 
   onClick() {
